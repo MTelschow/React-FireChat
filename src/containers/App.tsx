6e40007a5cbd240d44a3fdd16aa5061237709817
Mathis Telschow <mathistelschow@gmail.com>
2023-07-19 23:02:23 +0200

    Show messanges from database

--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -1,5 +1,7 @@
 import './App.css';
 
+import { Data } from 'react-firebase-hooks/firestore/dist/firestore/types';
+
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
@@ -21,15 +23,60 @@ const auth = firebase.auth();
 const firestore = firebase.firestore();
 
 function App() {
+	const [user] = useAuthState(auth);
+
 	return (
 		<>
 			<header className='App-header'></header>
 
-			<section>
-				
-			</section>
+			<section>{user ? <ChatRoom /> : <SignIn />}</section>
 		</>
 	);
 }
 
+function SignIn() {
+	const signInWithGoogle = () => {
+		const provider = new firebase.auth.GoogleAuthProvider();
+		auth.signInWithPopup(provider);
+	};
+
+	return <button onClick={signInWithGoogle}>Sign in with Google</button>;
+}
+
+function SignOut() {
+	return (
+		auth.currentUser && <button onClick={() => auth.signOut()}>Sign Out</button>
+	);
+}
+
+function ChatRoom() {
+	const messagesRef = firestore.collection('messages');
+	const query = messagesRef.orderBy('createdAt').limit(25);
+	const [messages] = useCollectionData(query, { idField: 'id' });
+	console.log(messages)
+	return (
+		<>
+			<SignOut />
+			<div>
+				{messages &&
+					messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
+			</div>
+			
+		</>
+	);
+}
+
+function ChatMessage(props: {
+	message: Data<firebase.firestore.DocumentData, '', ''>;
+}) {
+	const { text, uid, photoURL } = props.message;
+	const messageClass = uid ===auth.currentUser?.uid ? "sent" : "received";
+
+	return (
+		<div className={`message ${messageClass}`}>
+			<img src={photoURL} alt="User Photo" />
+			<p>{text}</p>
+		</div>
+	);
+}
 export default App;
